2f0f39b5fb459f1e171352ca0cbeca595ca6a367
William Riley <wrileystl@gmail.com>
2015-07-27 23:09:21 -0500

    working rainbox cycle and status reports to Zabbix

--- firmware/iMailbox/iMailbox.ino
+++ firmware/iMailbox/iMailbox.ino
@@ -9,6 +9,18 @@
 #include <Adafruit_NeoPixel.h>
 #include "localconfig.h"
 
+#define DEBUG
+
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)     Serial.print (x)
+ #define DEBUG_PRINTDEC(x)     Serial.print (x, DEC)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+ #define DEBUG_PRINT(x)
+ #define DEBUG_PRINTDEC(x)
+ #define DEBUG_PRINTLN(x) 
+#endif
+
 #define LEDPIN 0
 #define NEOPIN 2
 #define BATTCHARGEPIN 4
@@ -28,22 +40,21 @@ const int serverport = MY_SERVERPORT;
 
 char host[] = MY_HOST;
 char base64host[200];
+char base64key[200];
+char base64value[200];
 char key1[] = "lightReading";
-char base64key1[200];
 char key2[] = "batteryStatus";
-char base64key2[200];
-char value1[10];
-char base64value1[200];
-char value2[10];
-char base64value2[200];
-
+char key3[] = "uptime";
+char value[10];
 
 WiFiClient client;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, NEOPIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t setColor = strip.Color(0,64,0);
-int lightReading = 0;
+uint16_t lightReading = 0;
 uint8_t batteryStatus = 0;
+uint16_t elapsedSeconds = 0;
+uint32_t uptime = 0;
 
 void setup() {
   delay(500);
@@ -59,78 +70,125 @@ void setup() {
   pinMode(BATTCHARGEPIN, INPUT_PULLUP);
   pinMode(BATTDONEPIN, INPUT_PULLUP);
 
-  Serial.println();
-  Serial.println();
-  Serial.println("iMailbox starting");
+  DEBUG_PRINTLN();
+  DEBUG_PRINTLN();
+  DEBUG_PRINTLN("iMailbox starting");
 
+/*
   // read stored color
   EEPROM.begin(8);
   setColor = EEPROMReadlong(EEPROM_COLOR);
-  Serial.print("Read setColor: ");
-  Serial.println(setColor, DEC);
+  DEBUG_PRINT("Read setColor: ");
+  DEBUG_PRINTDEC(setColor);
+  DEBUG_PRINTLN();
   if(setColor == 4294967295) {
-    Serial.println("lastColor not set, writing default to EEPROM");
+    DEBUG_PRINTLN("Default color not set, writing default to EEPROM");
     setColor = strip.Color(0,64,0);
     EEPROMWritelong(EEPROM_COLOR, setColor);
   }
 
   // show color
-  Serial.println("Enabling LED strip");
+*/
+  DEBUG_PRINTLN("Enabling LED strip");
   strip.begin();
-  Serial.println("Setting LED color");
+/*
+  DEBUG_PRINTLN("Setting LED color");
   colorWipe(setColor, 1);
-
-  // get light reading
-  Serial.println("Getting light reading");
-  lightReading = analogRead(A0);
-  Serial.print("Read raw value: ");
-  Serial.print(lightReading, DEC);
-  Serial.println();
-
-  // get battery status
-  Serial.println("Getting battery status");
-  uint8_t battChg = !digitalRead(BATTCHARGEPIN);
-  uint8_t battDone = !digitalRead(BATTDONEPIN);
-  batteryStatus = battDone + (battChg * 2);
-  Serial.print("Read raw values CHARGE:");
-  Serial.print(battChg, DEC);
-  Serial.print(" DONE:");
-  Serial.print(battDone, DEC);
-  Serial.println();
+*/
 
   // connect to wifi
-  Serial.println("Connecting to WiFi");
+  DEBUG_PRINTLN("Connecting to WiFi");
   connectWifi(ssid, password);
 
   delay(1000);
+
+  DEBUG_PRINTLN("Done with setup()");
+  LEDOff();
+
+  DEBUG_PRINTLN("Sending initial status");
+  sendStatus();
   
+//  DEBUG_PRINTLN("Going to sleep in 1 second");
+//  delay(1000);
+//  ESP.deepSleep(SLEEPSECONDS * 1000000);
+}
+
+void loop() {
+  
+  uint16_t i, j, k;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    for(k=0; k < 10; k++) {
+      uptimeTick();
+      if(elapsedSeconds++ >= 60) {
+        sendStatus();
+        elapsedSeconds = 0;
+      }
+      delay(1000);
+    }
+  }
+}
+
+void uptimeTick() {
+  uptime++;
+}
+
+void sendStatus() {
+  LEDOn();
+  // get light reading
+  DEBUG_PRINT("sendStatus() at uptime of ");
+  DEBUG_PRINTDEC(uptime);
+  DEBUG_PRINTLN(" seconds");
+  DEBUG_PRINTLN("Getting light reading");
+  lightReading = analogRead(A0);
+  delay(10);
+  DEBUG_PRINT("Read raw value: ");
+  DEBUG_PRINTDEC(lightReading);
+  DEBUG_PRINTLN();
+
+  // get battery status
+  DEBUG_PRINTLN("Getting battery status");
+  uint8_t battChg = !digitalRead(BATTCHARGEPIN);
+  uint8_t battDone = !digitalRead(BATTDONEPIN);
+  batteryStatus = battDone + (battChg * 2);
+  DEBUG_PRINT("Read raw values CHARGE:");
+  DEBUG_PRINTDEC(battChg);
+  DEBUG_PRINT(" DONE:");
+  DEBUG_PRINTDEC(battDone);
+  DEBUG_PRINTLN();
+
   // send lightReading
   for(int i = 0; i < RETRIES; i++) {
-    Serial.print("Sending lightReading to Zabbix, try ");
-    Serial.print(i + 1, DEC);
-    Serial.print(" of ");
-    Serial.println(RETRIES, DEC);
+    DEBUG_PRINT("Sending lightReading to Zabbix, try ");
+    DEBUG_PRINTDEC(i + 1);
+    DEBUG_PRINT(" of ");
+    DEBUG_PRINTDEC(RETRIES);
+    DEBUG_PRINTLN();
     
     int connectResult = client.connect(server, serverport);
     if(connectResult) {
-      Serial.println("Connected to server");
-	  
+      DEBUG_PRINTLN("Connected to server");
+    
       base64_encode(base64host, host, sizeof(host)-1);
-	    base64_encode(base64key1, key1, sizeof(key1)-1);
-      itoa(lightReading,value1,sizeof(value1));
-	    base64_encode(base64value1, value1, sizeof(value1)-1);
-	  
-  	  String s = "<req>\n <host>";
-  	  s += base64host;
-  	  s += "</host>\n <key>";
-  	  s += base64key1;
-  	  s += "</key>\n <data>";
-  	  s += base64value1;
-  	  s += "</data>\n</req>\n";
-  	  client.print(s);
-  	  delay(10);
+      base64_encode(base64key, key1, sizeof(key1)-1);
+      itoa(lightReading,value,sizeof(value));
+      base64_encode(base64value, value, sizeof(value)-1);
+    
+      String s = "<req>\n <host>";
+      s += base64host;
+      s += "</host>\n <key>";
+      s += base64key;
+      s += "</key>\n <data>";
+      s += base64value;
+      s += "</data>\n</req>\n";
+      client.print(s);
+      delay(10);
       client.stop();
-      Serial.println("lightReading sent");
+      DEBUG_PRINTLN("lightReading sent");
       break;
     } else {
       delay(1000);
@@ -139,85 +197,106 @@ void setup() {
 
     // send batteryStatus
   for(int i = 0; i < RETRIES; i++) {
-    Serial.print("Sending batteryStatus to Zabbix, try ");
-    Serial.print(i + 1, DEC);
-    Serial.print(" of ");
-    Serial.println(RETRIES, DEC);
+    DEBUG_PRINT("Sending batteryStatus to Zabbix, try ");
+    DEBUG_PRINTDEC(i + 1);
+    DEBUG_PRINT(" of ");
+    DEBUG_PRINTDEC(RETRIES);
+    DEBUG_PRINTLN();
     
     int connectResult = client.connect(server, serverport);
     if(connectResult) {
-      Serial.println("Connected to server");
+      DEBUG_PRINTLN("Connected to server");
     
       base64_encode(base64host, host, sizeof(host)-1);
-      base64_encode(base64key2, key2, sizeof(key2)-1);
-      itoa(batteryStatus,value2,sizeof(value2));
-      base64_encode(base64value2, value2, sizeof(value2)-1);
+      base64_encode(base64key, key2, sizeof(key2)-1);
+      itoa(batteryStatus,value,sizeof(value));
+      base64_encode(base64value, value, sizeof(value)-1);
     
       String s = "<req>\n <host>";
       s += base64host;
       s += "</host>\n <key>";
-      s += base64key2;
+      s += base64key;
       s += "</key>\n <data>";
-      s += base64value2;
+      s += base64value;
       s += "</data>\n</req>\n";
       client.print(s);
       delay(10);
       client.stop();
-      Serial.println("batteryStatus sent");
+      DEBUG_PRINTLN("batteryStatus sent");
       break;
     } else {
       delay(1000);
     }
   }
 
-  //
-  // disconnect from wifi
-  // ******* This is disabled because it causes a reset **************
-//  Serial.println("Disconnecting WiFi");
-//  WiFi.disconnect();
+  // send uptime
+  for(int i = 0; i < RETRIES; i++) {
+    DEBUG_PRINT("Sending uptime to Zabbix, try ");
+    DEBUG_PRINTDEC(i + 1);
+    DEBUG_PRINT(" of ");
+    DEBUG_PRINTDEC(RETRIES);
+    DEBUG_PRINTLN();
+    
+    int connectResult = client.connect(server, serverport);
+    if(connectResult) {
+      DEBUG_PRINTLN("Connected to server");
+    
+      base64_encode(base64host, host, sizeof(host)-1);
+      base64_encode(base64key, key3, sizeof(key3)-1);
+      itoa(uptime,value,sizeof(value));
+      base64_encode(base64value, value, sizeof(value)-1);
+    
+      String s = "<req>\n <host>";
+      s += base64host;
+      s += "</host>\n <key>";
+      s += base64key;
+      s += "</key>\n <data>";
+      s += base64value;
+      s += "</data>\n</req>\n";
+      client.print(s);
+      delay(10);
+      client.stop();
+      DEBUG_PRINTLN("uptime sent");
+      break;
+    } else {
+      delay(1000);
+    }
+  }
 
-  Serial.println("Done with setup()");
   LEDOff();
-  
-  Serial.println("Going to sleep in 1 second");
-  delay(1000);
-  ESP.deepSleep(SLEEPSECONDS * 1000000);
-}
-
-void loop() {
 }
 
 // Fucntion to connect WiFi
 void connectWifi(const char* ssid, const char* password) {
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
+  DEBUG_PRINT("Connecting to ");
+  DEBUG_PRINT(ssid);
   WiFi.disconnect();
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(".");
+    DEBUG_PRINT(".");
   }
 
-  Serial.println("");
+  DEBUG_PRINTLN("");
   printWifiStatus();
 }
 
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+  DEBUG_PRINT("SSID: ");
+  DEBUG_PRINTLN(WiFi.SSID());
 
   // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  DEBUG_PRINT("IP Address: ");
+  DEBUG_PRINTLN(ip);
 
   // print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+  DEBUG_PRINT("signal strength (RSSI):");
+  DEBUG_PRINT(rssi);
+  DEBUG_PRINTLN(" dBm");
 }
 
 void LEDOn() {
@@ -226,76 +305,6 @@ void LEDOn() {
 
 void LEDOff() {
   digitalWrite(LEDPIN, 1);
-}
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
-  }
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
-        }
-    }
-  }
 }
 
 // Input a value 0 to 255 to get a color value.
