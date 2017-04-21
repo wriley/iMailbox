e2fa8279b7cd6aecdabf60a7d6ec80db60b71208
William Riley <wrileystl@gmail.com>
2017-04-21 10:19:19 -0500

    added firmware update options to config.h

--- firmware/iMailbox-base/src/config.h
+++ firmware/iMailbox-base/src/config.h
@@ -5,11 +5,14 @@
 #define WIFI_CLIENTSSID     "MYSSID"
 #define WIFI_CLIENTPASSWORD "MYPASSWD"
 #define WIFI_AP_NAME        "iMailbox"
-#define WIFI_AP_PASSWORD    "00000000"
+#define WIFI_AP_PASSWORD    "123456"
 #define PLATFORM_DEBUG      true
 #define TCPSERVERIP         "10.10.10.100"
 #define TCPSERVERPORT       10051
 #define WSGPIO              2
 #define WSPIXELS            12
+#define UPDATE_PATH "/firmware"
+#define UPDATE_USERNAME  "admin"
+#define UPDATE_PASSWORD  "admin"
 
 #endif
